9b4a8b67be71fcb693bc4d8f99d9b4cee5406442
foonoxous <RWQe4qDs7LIsmI7Q94ctNMSfwfDW8eld24YGevEzQEKZyfJz1NPlH6uf>
2021-11-14 17:51:05 +0000

    Fix deprecation warning

--- covert/__init__.py
+++ covert/__init__.py
@@ -1,7 +1,7 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-  __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+  __version__ = version(__name__)
+except PackageNotFoundError:
   # package is not installed
   pass
