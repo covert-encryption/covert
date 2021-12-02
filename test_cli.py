1ec0fb5c36789853541ab7728958ae950012e1d6
rocketdey <94972168+rocketdey@users.noreply.github.com>
2021-12-02 19:14:46 +0300

    Add files via upload

---
+++ test_cli.py
@@ -0,0 +1,24 @@
+import pytest
+import sys
+
+def test_argparser(capsys):
+  from covert.__main__ import argparse  # Import *after* capsys wraps stdout/stderr
+  
+  # Correct but complex arguments
+  sys.argv = "covert enc --recipient recipient1 -r recipient2 -Arrp recipient3 recipient4".split()
+  a = argparse()
+  assert a.recipients == ['recipient1', 'recipient2', 'recipient3', 'recipient4']
+  assert a.paste is True
+  assert a.askpass == 1
+  # Should produce no output
+  cap = capsys.readouterr()
+  assert not cap.out
+  assert not cap.err
+
+  # Missing argument parameter
+  sys.argv = "covert enc -Arrp recipient1".split()
+  with pytest.raises(SystemExit):
+    argparse()
+  cap = capsys.readouterr()
+  assert not cap.out
+  assert "Argument parameter missing: covert enc -Arrp …" in cap.err
