94f872821042cbfc11ce587d84be4fb3ebdf7b25
rocketdey <94972168+rocketdey@users.noreply.github.com>
2021-12-06 20:06:55 +0300

    Add files via upload

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,8 +2,8 @@ import pytest
 import sys
 
 def test_argparser(capsys):
-  from covert.__main__ import argparse  # Import *after* capsys wraps stdout/stderr
-  
+  from covert.__main__ import argparse
+
   # Correct but complex arguments
   sys.argv = "covert enc --recipient recipient1 -r recipient2 -Arrp recipient3 recipient4".split()
   a = argparse()
@@ -15,6 +15,15 @@ def test_argparser(capsys):
   assert not cap.out
   assert not cap.err
 
+  # Giving mode within combined arguments
+  sys.argv = "covert -eArrp recipient1 recipient2".split()
+  a = argparse()
+  assert a.recipients == ['recipient1', 'recipient2']
+  assert a.askpass == 1
+  assert a.paste is True
+  cap = capsys.readouterr()
+  assert not cap.out
+
   # Missing argument parameter
   sys.argv = "covert enc -Arrp recipient1".split()
   with pytest.raises(SystemExit):
@@ -22,3 +31,11 @@ def test_argparser(capsys):
   cap = capsys.readouterr()
   assert not cap.out
   assert "Argument parameter missing: covert enc -Arrp …" in cap.err
+  # For combined mode
+  sys.argv = "covert -eArrp recipient1".split()
+  with pytest.raises(SystemExit):
+    argparse()
+  cap = capsys.readouterr()
+  assert not cap.out
+  assert "Argument parameter missing: covert enc -Arrp …" in cap.err
+
