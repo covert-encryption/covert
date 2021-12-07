e2cacb6e73619c924f9ab922ae1ebf0a39d62d44
covert-encryption <93869732+covert-encryption@users.noreply.github.com>
2021-12-07 14:25:20 +0000

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 
 def test_argparser(capsys):
-  from covert.__main__ import argparse
+  from covert.__main__ import argparse  # Import only *after* capsys wraps sys.stdout/stderr
 
   # Correct but complex arguments
   sys.argv = "covert enc --recipient recipient1 -r recipient2 -Arrp recipient3 recipient4".split()
@@ -31,6 +31,7 @@ def test_argparser(capsys):
   cap = capsys.readouterr()
   assert not cap.out
   assert "Argument parameter missing: covert enc -Arrp …" in cap.err
+
   # For combined mode
   sys.argv = "covert -eArrp recipient1".split()
   with pytest.raises(SystemExit):
