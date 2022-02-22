c9c2237adb9029718b294af7f2af914cb65b9a69
foonoxous <RWQe4qDs7LIsmI7Q94ctNMSfwfDW8eld24YGevEzQEKZyfJz1NPlH6uf>
2022-02-22 22:33:49 +0000

    Ratchet working in principle.

--- covert/ratchet.py
+++ covert/ratchet.py
@@ -22,85 +22,104 @@ class Ratchet:
     self.skipped = []
 
   def init_alice(self, localkey, peerkey):
+    """Prepare Alice for sending initial message(s) to Bob using his public key."""
     self.root = derive_symkey(b"ratchet/init", localkey, peerkey)
+    _, self.nh_recv = chainstep(self.root, b"hkey")
     self.localkey = localkey
     self._tock(peerkey)
 
-  def init_bob(self, localkey, peerkey):
+  def init_bob(self, localkey, ciphertext):
+    """Bob receives an initial message from Alice, initialise ratchet on Bob side for replies."""
+    ephkey = Key(pkhash=ciphertext[:32])
+    key = derive_symkey(ephkey.pkhash[:12], localkey, ephkey)
+    header = decrypt(ciphertext[32:83], None, b"ratchet/init", key)
+    peerkey = Key(pk=header[:32])
+    N = int.from_bytes(header[32:34], "little")
     self.root = derive_symkey(b"ratchet/init", localkey, peerkey)
+    _, self.nh_send = chainstep(self.root, b"hkey")  # Matches Alice's initial nh_recv
     self.localkey = localkey
-    self._tick(peerkey)
+    self.skip_until(N)
+    self.dhstep(peerkey)
+    self.chain_recv, mk = chainstep(self.chain_recv)
+    self.Nr += 1
+    return mk
 
   def dhstep(self, peerkey):
     """ECDH ratchet step, when message with new header key is received."""
     self.PN = self.Ns
     self.Ns = self.Nr = 0
-    self.h_send = self.nh_send
     self.h_recv = self.nh_recv
     self._tick(peerkey)
+    # Right here we should be in sync with the peer.
     self.localkey = Key()
+    self.h_send = self.nh_send
     self._tock(peerkey)
 
   def _tick(self, peerkey):
-    self.root, self.chain_recv = chainstep(self.root, derive_symkey(b"", self.localkey, peerkey))
+    """DH update receiving keys"""
+    self.root, self.chain_recv = chainstep(self.root, derive_symkey(b"ratchet", self.localkey, peerkey))
     _, self.nh_recv = chainstep(self.root, b"hkey")
 
   def _tock(self, peerkey):
-    self.root, self.chain_send = chainstep(self.root, derive_symkey(b"", self.localkey, peerkey))
+    """DH update sending keys"""
+    self.root, self.chain_send = chainstep(self.root, derive_symkey(b"ratchet", self.localkey, peerkey))
     _, self.nh_send = chainstep(self.root, b"hkey")
 
-  def send(self, peerkey):
+  def send(self, peerkey=None):
     f = 1  # Flags: 1 means signature
     if not self.chain_recv:
+      # Alice initial message format
       ephkey = Key()
-      key = derive_symkey(ephkey.pkhash[:12], self.localkey, peerkey)
+      key = derive_symkey(ephkey.pkhash[:12], ephkey, peerkey)
       header = ephkey.pkhash + encrypt(self.localkey.pk + (self.Ns & 0xFFFF).to_bytes(2, "little") + f.to_bytes(1, "little"), None, b"ratchet/init", key)
     else:
+      # Normal ratchet message
       header = encrypt(self.localkey.pk + self.PN.to_bytes(2, "little") + f.to_bytes(1, "little"), None, self.Ns.to_bytes(12, "little"), self.h_send)
     self.chain_send, mk = chainstep(self.chain_send)
     self.Ns += 1
     return header, mk
 
   def receive(self, ciphertext):
-    hkey = self.h_recv
     # Try skipped keys
     for [hkey, n, mk] in self.skipped:
       with suppress(CryptoError):
-        header = decrypt(ciphertext[:35 + 16], None, n.to_bytes(12, "little"), self.h_recv)
+        if hkey:
+          # Normal ratchet messages
+          header = decrypt(ciphertext[:51], None, n.to_bytes(12, "little"), hkey)
+        else:
+          # Alice's initial messages (but Bob is already initialised)
+          ephkey = Key(pkhash=ciphertext[:32])
+          key = derive_symkey(ephkey.pkhash[:12], localkey, ephkey)
+          header = decrypt(ciphertext[32:83], None, b"ratchet/init", key)
         self.skipped.remove([hkey, n, mk])
         return mk
+    header = None
+    # Try with current header key
     if self.h_recv:
-      # Try with old header key
       for n in range(self.Nr, self.Nr + 20):
         with suppress(CryptoError):
-          header = decrypt(ciphertext[:35 + 16], None, n.to_bytes(12, "little"), self.h_recv)
-          self.skip_messages(n)
-    else:
-      # Try with next header key
+          header = decrypt(ciphertext[:51], None, n.to_bytes(12, "little"), self.h_recv)
+          self.skip_until(n)
+          break
+    # Try with next header key
+    if not header:
       for n in range(20):
         with suppress(CryptoError):
-          header = decrypt(ciphertext[:35 + 16], None, n.to_bytes(12, "little"), self.nh_recv)
+          header = decrypt(ciphertext[:51], None, n.to_bytes(12, "little"), self.nh_recv)
           PN = int.from_bytes(header[32:34], "little")
           self.skip_until(PN)
           self.dhstep(Key(pk=header[:32]))
           self.skip_until(n)
+    if not header:
+      raise CryptoError("Unable to authenticate")
+    # Advance receiving chain
     self.chain_recv, mk = chainstep(self.chain_recv)
     self.Nr += 1
     return mk
 
   def skip_until(self, n):
+    """Advance the receiving chain across all messages prior to message n."""
     while self.Nr < n:
       self.chain_recv, mk = chainstep(self.chain_recv)
-      skipped.push([self.h_recv, self.Nr, mk])
+      self.skipped.append([self.h_recv, self.Nr, mk])
       self.Nr += 1
-
-# Header prior to first ratchet reply (Alice's initial messages)
-# nonce = b"ratchet/init"
-# pubkey:   sha(nonce + dh(eph, idB))
-# x3dh:     sha(dh(eph, idB) + dh(eph, skB) [+ dh(eph, otB)])
-# root key = dhstep(idA, skB or idB)
-#[eph:32]{[pk:32][n:2][f:1][nextlen:3]}[tag:16]
-
-# Normal ratchet header
-# nonce = Ns
-#{[dh:32][pn:2][f:1][nextlen:3]}[tag:16]
--- tests/test_ratchet.py
+++ tests/test_ratchet.py
@@ -7,9 +7,30 @@ def test_ratchet_pubkey():
   a = Ratchet()
   a.init_alice(alice, bob)
   header, mka = a.send(bob)
+  assert len(header) == 83
+  assert mka
+  assert a.Ns == 1
+  assert not a.skipped
+  assert not a.h_send
+  assert a.nh_send
 
   b = Ratchet()
-  b.init_bob(bob, alice)
-  mkb = b.receive(header)
-
+  mkb = b.init_bob(bob, header)
   assert mka == mkb
+  assert b.Nr == 1
+  assert not b.skipped
+  assert b.h_send
+  assert b.h_send == a.nh_recv
+
+  header2, mkb2 = b.send()
+  assert mkb2 != mkb
+
+  header3, mkb3 = b.send()
+  assert mkb3 != mkb2
+
+  # Receive out of order
+  mka3 = a.receive(header3)
+  assert mka3 == mkb3
+
+  mka2 = a.receive(header2)
+  assert mka2 == mkb2
